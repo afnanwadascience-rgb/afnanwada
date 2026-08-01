eb49f199978b28472bd7d8fa75e613b732bef989
myai <afnanwada445778@gmail.com>
2026-08-01 22:52:58 +0600

    add sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,1 @@
+import { MetadataRoute } from 'next' export default function sitemap(): MetadataRoute.Sitemap { return [ { url: 'https://afnanwada-iota.vercel.app', lastModified: new Date(), changeFrequency: 'daily', priority: 1, }, ] }
